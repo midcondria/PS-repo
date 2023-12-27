da1b0a06b8a14dc4452cbc44a43a1a515ba5ab60
midcondria <119824778+midcondria@users.noreply.github.com>
2023-12-27 22:47:49 +0900

    Create 모음 제거.js
    프로그래머스 코딩테스트 입문

---
+++ programmers/Level0/모음 제거.js
@@ -0,0 +1,7 @@
+function solution(my_string) {
+    let moum = ['a', 'e', 'i', 'o', 'u'];
+    for (let m of moum) {
+        my_string = my_string.replaceAll(m, "");
+    }
+    return my_string;
+}
