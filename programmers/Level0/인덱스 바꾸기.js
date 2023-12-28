180fa98891fd29af287b780bad20242715242d4a
midcondria <119824778+midcondria@users.noreply.github.com>
2023-12-28 11:13:01 +0900

    Create 인덱스 바꾸기.js
    프로그래머스 코딩테스트 입문

---
+++ programmers/Level0/인덱스 바꾸기.js
@@ -0,0 +1,7 @@
+function solution(my_string, num1, num2) {
+    let arr = my_string.split("");
+    arr[num1] = my_string.at(num2);
+    
+    arr[num2] = my_string.at(num1);
+    return arr.join("");
+}
